81dcc7912ce7d09457fc12e2fb3bc923109f5bcf
VasanthaUma <buddigav@gmail.com>
2018-10-30 13:09:57 -0400

    Add project files.

---
+++ Assignment3/Source.cpp
@@ -0,0 +1,39 @@
+//Developed by Vasantha Buddiga
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+	system("color F0");
+	char word[50];
+	cout << "Enter word:" << endl;
+	cin >> word;
+
+	//initialize variables
+	int i = 0;
+	int j = strlen(word) - 1;
+
+	/*AS string length saves the null character
+	therefore -1
+	initialize a boolean variable
+	*/
+	bool a = true;
+	for (i = 0; i < j; i++)
+	{
+		if (word[i]!= word[j])
+		{
+			a = false;
+		}
+		j--;
+	}
+	if (a)
+	{
+		cout << "Yes the word is palindrome"<<endl;
+	}
+	else
+	{
+		cout << "Not Palindrome"<<endl;
+	}
+	system("pause");
+	return 0;
+}
