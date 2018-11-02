03cb93f18b13fcec9772ea139105dd64ed5e8f4e
VasanthaUma <buddigav@gmail.com>
2018-11-01 23:41:21 -0400

    FunctionAssignment3

--- Assignment3/Source.cpp
+++ Assignment3/Source.cpp
@@ -2,25 +2,36 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+
+bool isPalaindrome(char word[50]);
+
 int main()
 {
 	system("color F0");
 	char word[50];
 	cout << "Enter word:" << endl;
 	cin >> word;
+	isPalaindrome(word);
+	system("pause");
+	return 0;
+}
+	
 
-	//initialize variables
-	int i = 0;
-	int j = strlen(word) - 1;
-
+bool isPalaindrome(char word[50])
+{
+	
 	/*AS string length saves the null character
 	therefore -1
 	initialize a boolean variable
 	*/
+
+	int i = 0;
+	int j = strlen(word) - 1;
+
 	bool a = true;
 	for (i = 0; i < j; i++)
 	{
-		if (word[i]!= word[j])
+		if (word[i] != word[j])
 		{
 			a = false;
 		}
@@ -28,12 +39,14 @@ int main()
 	}
 	if (a)
 	{
-		cout << "Yes the word is palindrome"<<endl;
+		cout << "Yes the word is palindrome" << endl;
 	}
 	else
 	{
-		cout << "Not Palindrome"<<endl;
+		cout << "Not Palindrome" << endl;
 	}
-	system("pause");
-	return 0;
+	return a;
+
 }
+
+
